94727453653d23d7b47506a7e5f07fffd55494b1
authcompanion <paul@pmprosociety.com>
2022-09-14 20:33:21 -0400

    fix: updated config to ESM

--- documentation/docs/.vuepress/config.js
+++ documentation/docs/.vuepress/config.js
@@ -1,44 +1,53 @@
-const { defaultTheme } = require('vuepress')
-module.exports = {
-    lang: "en-US",
-    title: "AuthCompanion 2",
-    description: "Documenation Site for AuthCompanion - An effortless, open source, token-based user management server - well suited for modern web projects.",
-    // head: [['script', { defer: true, src: 'https://t.jitsu.com/s/lib.js', 'data-key': 'js.70qf40hrc3y266v4uud9dh.fub39cgaa9a45y8iqpteqq', 'data-tracking-host': 'https://young-thunder-2379.fly.dev/' }]],
-    theme: defaultTheme({
-      logo: "",
-      darkMode: true,
-      repo: "authcompanion/authcompanion2",
-      editLink: false,
-      lastUpdated: true,
-      navbar: [
+import { defineUserConfig } from "vuepress";
+import { defaultTheme } from 'vuepress'
+
+export default defineUserConfig({
+  lang: "en-US",
+  title: "AuthCompanion 2",
+  description:
+    "Documenation Site for AuthCompanion - An effortless, open source, token-based user management server - well suited for modern web projects.",
+  // head: [['script', { defer: true, src: 'https://t.jitsu.com/s/lib.js', 'data-key': 'js.70qf40hrc3y266v4uud9dh.fub39cgaa9a45y8iqpteqq', 'data-tracking-host': 'https://young-thunder-2379.fly.dev/' }]],
+  theme: defaultTheme({
+    logo: "",
+    darkMode: true,
+    repo: "authcompanion/authcompanion2",
+    editLink: false,
+    lastUpdated: true,
+    navbar: [
+      {
+        text: "Guide",
+        link: "/",
+      },
+      {
+        text: "Contributing",
+        link: "/contributing/",
+      },
+      {
+        text: "License",
+        link: "/guide/licenseoverview.md",
+      },
+    ],
+    // sidebar object
+    // pages under different sub paths will use different sidebar
+    sidebar: {
+      "/": [
         {
-          text: "Guide",
-          link: "/",
-        },
-        {
-          text: "Contributing",
-          link: "/contributing/",
-        },
-        {
-          text: "License",
-          link: "/guide/licenseoverview.md"
+          text: "📚 The Guide",
+          children: [
+            "/guide/gettingstarted.md",
+            "/guide/explore.md",
+            "/guide/configuration.md",
+            "/guide/webforms.md",
+            "/guide/authapi.md",
+          ],
         },
       ],
-      // sidebar object
-      // pages under different sub paths will use different sidebar
-      sidebar: {
-        "/": [
-          {
-            text: "📚 The Guide",
-            children: ["/guide/gettingstarted.md", "/guide/explore.md", "/guide/configuration.md", "/guide/webforms.md", "/guide/authapi.md"],
-          },
-        ],
-        "/contributing/": [
-          {
-            text: "How to Contribute",
-            children: ["gettinghelp.md", "codeofconduct.md"],
-          },
-        ],
-      },
-    }),
-  };
+      "/contributing/": [
+        {
+          text: "How to Contribute",
+          children: ["gettinghelp.md", "codeofconduct.md"],
+        },
+      ],
+    },
+  }),
+});

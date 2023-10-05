97dd115508db5e0995eb28b6431eabb48c2f4d61
authcompanion <paul@pmprosociety.com>
2023-10-04 21:02:58 -0400

    feat: admin credentials are written to file and console on authc's first start

--- plugins/key/admin.key.js
+++ plugins/key/admin.key.js
@@ -1,29 +1,31 @@
-import fastifyPlugin from 'fastify-plugin';
-import config from '../../config.js';
-import { writeFileSync } from 'fs';
-import { createId } from '@paralleldrive/cuid2';
-import { createHash } from '../../utils/credential.js';
-import * as crypto from 'crypto';
+import fastifyPlugin from "fastify-plugin";
+import config from "../../config.js";
+import { writeFileSync } from "fs";
+import { createId } from "@paralleldrive/cuid2";
+import { createHash } from "../../utils/credential.js";
+import * as crypto from "crypto";
 
 //function to generate a random password using crypto module
 const generatePassword = function () {
-  return crypto.randomBytes(20).toString('hex');
+  return crypto.randomBytes(20).toString("hex");
 };
 
 const setupAdminKey = async function (fastify) {
   try {
     //create test database to support CI
-    if (process.env.NODE_ENV === 'test') {
-      config.ADMINKEYPATH = './adminkey_test';
+    if (process.env.NODE_ENV === "test") {
+      config.ADMINKEYPATH = "./adminkey_test";
     }
 
     //Check if the admin user already exists on server startup
-    const stmt = fastify.db.prepare('SELECT uuid, name, email, active, created_at, updated_at FROM admin LIMIT 1;');
+    const stmt = fastify.db.prepare(
+      "SELECT uuid, name, email, active, created_at, updated_at FROM admin LIMIT 1;"
+    );
     const adminUser = await stmt.get();
 
     if (adminUser) {
       //register the admin user on the fastify instance
-      fastify.decorate('registeredAdminUser', adminUser);
+      fastify.decorate("registeredAdminUser", adminUser);
       fastify.log.info(`Using Admin API Key: ${config.ADMINKEYPATH}`);
       return;
     }
@@ -36,29 +38,39 @@ const setupAdminKey = async function (fastify) {
     //Create a default admin account
     const userObj = {
       uuid: uuid,
-      name: 'Admin',
-      email: 'admin@localhost',
+      name: "Admin",
+      email: "admin@localhost",
     };
 
     const registerStmt = fastify.db.prepare(
       "INSERT INTO admin (uuid, name, email, password, active, jwt_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, strftime('%Y-%m-%dT%H:%M:%fZ','now'), strftime('%Y-%m-%dT%H:%M:%fZ','now')) RETURNING uuid, name, email, active, created_at, updated_at;"
     );
-    const user = registerStmt.get(uuid, userObj.name, userObj.email, hashPwd, 1, '');
+    const user = registerStmt.get(
+      uuid,
+      userObj.name,
+      userObj.email,
+      hashPwd,
+      1,
+      ""
+    );
 
     //export admin password to a file. Admin password is only exported once on server startup and can be traded for an access token
-    writeFileSync(config.ADMINKEYPATH, `admin password: ${adminPwd}`);
+    writeFileSync(
+      config.ADMINKEYPATH,
+      `admin email: ${userObj.email} \nadmin password: ${adminPwd}`
+    );
     fastify.log.info(`Generating Admin API Key: ${config.ADMINKEYPATH}...`);
-    fastify.log.info(`Admin Password is: ${adminPwd}`);
+    fastify.log.info(`Admin Email: ${userObj.email} & Password: ${adminPwd}`);
 
     //register the admin user on the fastify instance
-    fastify.decorate('registeredAdminUser', user);
+    fastify.decorate("registeredAdminUser", user);
 
     fastify.log.info(`Using Admin API Key: ${config.ADMINKEYPATH}`);
   } catch (error) {
     console.log(error);
-    throw new Error('Failed to export the admin key');
+    throw new Error("Failed to export the admin key");
   }
 };
 
 //Wrap as Fastify Plugin
-export default fastifyPlugin(setupAdminKey, { fastify: '4.x' });
+export default fastifyPlugin(setupAdminKey, { fastify: "4.x" });

39e13f3cd5a0ee4c33cd4afe4efa96a63b84067b
authcompanion <paul@pmprosociety.com>
2023-11-08 07:57:14 -0500

    fix: more robust config

--- config.js
+++ config.js
@@ -2,12 +2,11 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  AUTHPORT: process.env.PORT || "3002",
+  AUTHPORT: parseInt(process.env.PORT, 10) || 3002,
   ORIGIN: process.env.ORIGIN || "http://localhost:3002",
   APPLICATIONORIGIN: process.env.APPLICATION_ORIGIN || "http://localhost:3002/v1/web/home",
-  REGISTRATIONORIGIN: process.env.REGISTRATION_ORIGIN
-    ? process.env.REGISTRATION_ORIGIN
-    : process.env.APPLICATION_ORIGIN || "http://localhost:3002/v1/web/home",
+  REGISTRATIONORIGIN:
+    process.env.REGISTRATION_ORIGIN || process.env.APPLICATION_ORIGIN || "http://localhost:3002/v1/web/home",
   ADMINORIGIN: process.env.ADMIN_ORIGIN || "http://localhost:3002/v1/admin/dashboard",
   DBPATH: process.env.DB_PATH || "./authcompanion_users.db",
   KEYPATH: process.env.SERVER_KEY_PATH || "./serverkey",
@@ -15,7 +14,7 @@ const config = {
   SAMESITE: process.env.SAMESITE || "None",
   RECOVERYURL: process.env.RECOVERY_REDIRECT_URL || "http://localhost:3002/v1/web/profile",
   SMTPHOSTNAME: process.env.SMTP_HOSTNAME || "",
-  SMTPPORT: process.env.SMTP_PORT || "2525",
+  SMTPPORT: parseInt(process.env.SMTP_PORT, 10) || 2525,
   SMTPUSER: process.env.SMTP_USERNAME || "",
   SMTPPASSWORD: process.env.SMTP_PASSWORD || "",
   FROMADDRESS: process.env.FROM_ADDRESS || "no_reply@example.com",
